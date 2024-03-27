1865a0f8d815cd36edfb2ade1cb25d77ddd8aaee
Carlos Zambrano <carlosmariozambrano2015@gmail.com>
2024-03-27 11:59:00 -0500

    minor bugs fixed at tic-tac-toe

--- odin-tic-tac-toe/app.js
+++ odin-tic-tac-toe/app.js
@@ -1,10 +1,10 @@
 // DOM elements
 const symbolSelection = document.querySelector(".symbol-selection");
 const gameBoard = document.querySelectorAll(".game-board div");
-const winMsg = document.querySelector('.winner-msg');
+const winMsg = document.querySelector(".winner-msg");
 
 const board = ["", "", "", "", "", "", "", "", ""];
-let symbol = 'O';
+let symbol = "O";
 let winner = false;
 
 function startGame() {
@@ -18,12 +18,16 @@ function startGame() {
           board[i] = symbol;
           nMoves++;
           winner = checkWinner(board, symbol);
-          if (winner) { winMsg.textContent = `the winner is: ${symbol}`; }
+          if (winner) {
+            winMsg.textContent = `the winner is: ${symbol}`;
+          }
           changeSymbol();
         }
       }
 
-      if (nMoves >= 9) { console.log("it's a tie"); }
+      if (nMoves >= 9 && !winner) {
+        winMsg.textContent = "it's a tie";
+      }
     });
   }
 }
@@ -33,18 +37,19 @@ function changeSymbol() {
 }
 
 function checkWinner(board, symbol) {
-  if ((board[0] == symbol && board[1] == symbol && board[2] == symbol) ||
+  if (
+    (board[0] == symbol && board[1] == symbol && board[2] == symbol) ||
     (board[3] == symbol && board[4] == symbol && board[5] == symbol) ||
     (board[6] == symbol && board[7] == symbol && board[8] == symbol) ||
     (board[0] == symbol && board[3] == symbol && board[6] == symbol) ||
     (board[1] == symbol && board[4] == symbol && board[7] == symbol) ||
     (board[2] == symbol && board[5] == symbol && board[8] == symbol) ||
     (board[0] == symbol && board[4] == symbol && board[8] == symbol) ||
-    (board[2] == symbol && board[4] == symbol && board[6] == symbol)) {
+    (board[2] == symbol && board[4] == symbol && board[6] == symbol)
+  ) {
     return true;
   }
   return false;
 }
 
 startGame();
- 
